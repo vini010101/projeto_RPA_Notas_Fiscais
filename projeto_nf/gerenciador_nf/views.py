8a903cbf23507a1590d36f35939754d6d6981f2b
Vinicius Calebe Barbosa de Moura <111473558+vini010101@users.noreply.github.com>
2025-01-03 13:45:24 -0300

    reformulei a pagina das viwes dexei ela mais enxuta

--- projeto_nf/gerenciador_nf/views.py
+++ projeto_nf/gerenciador_nf/views.py
@@ -6,46 +6,17 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 def login_view(request):
-    if request.method == 'POST':
-        # Inicializa o formulário com os dados enviados
-        form = AuthenticationForm(request, data=request.POST)
-        
-        if form.is_valid():
-            # Obtém os dados do formulário
-            nome_usuario = form.cleaned_data.get('username')
-            senha = form.cleaned_data.get('password')
-            
-            # Autentica o usuário
-            usuario = authenticate(username=nome_usuario, password=senha)
-            
-            if usuario is not None:
-                # Loga o usuário no sistema
-                login(request, usuario)
-                
-                # Redireciona para a página index.html
-                return redirect('index')
-        else:
-            # Se os dados forem inválidos, exibe o formulário com erros
-            return render(request, 'accounts/login.html', {'form': form})
-    else:
-        # Para requisições GET, exibe um formulário vazio
-        form = AuthenticationForm()
+    # Para requisições GET, exibe um formulário vazio
+    form = AuthenticationForm()
     
+    # Renderiza o template com o formulário
     return render(request, 'accounts/login.html', {'form': form})
 
+
+
 def usuario_view(request):
-    if request.method == 'POST':
-        # Inicializa o formulário com os dados enviados
-        form = UserCreationForm(request.POST)
-        
-        if form.is_valid():
-            # Cria o novo usuário
-            form.save()
-            # Redireciona para uma página de sucesso ou login
-            return redirect('login')  # Você pode ajustar a URL para o que desejar
-    else:
-        # Para requisições GET, exibe um formulário vazio
-        form = UserCreationForm()
+    # Para requisições GET, exibe um formulário vazio
+    form = UserCreationForm()
 
     # Renderiza o template com o formulário
     return render(request, 'accounts/criar_usuario.html', {'form': form})

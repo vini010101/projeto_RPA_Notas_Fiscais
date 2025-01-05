29c670c4a68a97d6a76d1cc9565ea736735730ed
Vinicius Calebe Barbosa de Moura <111473558+vini010101@users.noreply.github.com>
2025-01-05 14:11:01 -0300

    no momento o programa esta funcionando corrtamente as rotas e o sistema de autenticação foi implementado com sucesso.

--- projeto_nf/gerenciador_nf/models.py
+++ projeto_nf/gerenciador_nf/models.py
@@ -1,17 +1,42 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.hashers import make_password
 
-# Modelo de Usuário
-class Usuarios(models.Model):
+class UsuarioManager(BaseUserManager):
+    def ativo(self):
+        return self.filter(ativo=True)
+
+    def buscar_por_nome(self, nome_usuario):
+        return self.filter(nome_usuario=nome_usuario)
+
+    def criar_usuario(self, nome_usuario, senha):
+        """Método para criar um novo usuário com senha criptografada."""
+        senha_criptografada = make_password(senha)
+        usuario = self.create(
+            nome_usuario=nome_usuario,
+            senha=senha_criptografada
+        )
+        return usuario
+
+class Usuarios(AbstractBaseUser):
     id = models.AutoField(primary_key=True)
-    nome_usuario = models.CharField(max_length=100)
+    nome_usuario = models.CharField(max_length=100, unique=True)
     senha = models.CharField(max_length=100)
     data_criacao = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(default=timezone.now)
+    ativo = models.BooleanField(default=True)
+
+    objects = UsuarioManager()
+
+    USERNAME_FIELD = 'nome_usuario'
+    REQUIRED_FIELDS = ['senha']
 
     def __str__(self):
         return self.nome_usuario
 
 
-# Modelo de Nota Fiscal
+
 class NotaFiscal(models.Model):
     cpf_cnpj = models.CharField(max_length=18)
     valor_nota = models.DecimalField(max_digits=10, decimal_places=2)
@@ -24,7 +49,7 @@ class NotaFiscal(models.Model):
         return f"Nota Fiscal - {self.cpf_cnpj} - {self.valor_nota} - {self.data_nota}"
 
 
-# Modelo de Upload de Nota Fiscal
+
 class UploadNotaFiscal(models.Model):
     id = models.AutoField(primary_key=True)
     nota_fiscal = models.ForeignKey(NotaFiscal, on_delete=models.CASCADE, related_name="uploads")
